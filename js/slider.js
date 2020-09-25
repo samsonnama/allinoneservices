817aa35c247f068d76c06f28c3443078e912bafd
samsonnama <63802946+samsonnama@users.noreply.github.com>
2020-09-25 19:25:40 +0530

    Add files via upload

---
+++ js/slider.js
@@ -0,0 +1,20 @@
+/*jslint plusplus: true */
+/*global $, jQuery, console, alert, prompt */
+
+//$('.carousel').carousel();
+
+// Em An
+// 13-7-2017
+// https://codepen.io/collection/AGNJNB/
+// https://codepen.io/Dr_rakcha/pen/WOWrxr
+// https://codepen.io/Em-An/pen/NbQZjZ
+
+// https://codepen.io/mephysto/pen/ZYVKRY
+
+
+
+
+// https://codepen.io/Dr_rakcha/pen/WOWrxr
+
+
+// https://codepen.io/Em-An/pen/wjNKBP
